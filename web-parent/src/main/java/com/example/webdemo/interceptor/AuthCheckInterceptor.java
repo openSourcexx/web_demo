8e03b02fd8808dd5bb16980cc9319e7786d5c421
aq <andaid@163.com>
2019-03-26 20:30:03 +0800

    目录结构

--- web-parent/src/main/java/com/example/webdemo/interceptor/AuthCheckInterceptor.java
+++ web-parent/src/main/java/com/example/webdemo/interceptor/AuthCheckInterceptor.java
@@ -1,4 +1,4 @@
-package com.example.webdemo.common.interceptor;
+package com.example.webdemo.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- web-parent/src/main/java/com/example/webdemo/interceptor/InterceptConfig.java
+++ web-parent/src/main/java/com/example/webdemo/interceptor/InterceptConfig.java
@@ -1,4 +1,4 @@
-package com.example.webdemo.common.interceptor;
+package com.example.webdemo.interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
